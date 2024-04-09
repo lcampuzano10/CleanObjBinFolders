708559f06c6fa71442fec713046d7fbf86bd5432
Luis C Casa <luis.campuzano32@hotmail.com>
2024-04-09 15:54:33 -0400

    InitialCommit

---
+++ CleanObjBinFolder/DirectoryDeleting.cs
@@ -0,0 +1,88 @@
+﻿namespace CleanObjBinFolder
+{
+    public class DirectoryDeleting
+    {
+        public List<string> PathsToDelete { get; set; } = new();
+
+        public void RecursiveDirectory(string parentPath)
+        {
+            if (Directory.Exists(parentPath))
+            {
+                var findParents = Directory.GetDirectories(parentPath).Where(s => !s.Contains(".vs")).ToList();
+
+                foreach (var pathParent in findParents)
+                {
+                    var pathParentLower = pathParent.ToLower();
+                    if (!(pathParentLower.Contains("angular") || pathParentLower.Contains("react")))
+                    {
+                        ListOfDirectoriesDelete(pathParent, "obj");
+                        ListOfDirectoriesDelete(pathParent, "bin");
+                    }
+
+                    RecursiveDirectory(pathParent);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Directory does not exist");
+            }
+        }
+
+        public void ListOfDirectoriesDelete(string pathParent, string directoryToFind)
+        {
+            var fullPath = Path.Combine(pathParent, directoryToFind);
+
+            List<string> findDirectory = Directory.GetDirectories(pathParent).Where(x => x == fullPath).ToList();
+
+            if (findDirectory.Count() > 0)
+            {
+                foreach (var path in findDirectory)
+                {
+                    PathsToDelete.Add(path);
+                }
+            }
+        }
+
+        public static void DeleteDirectory(string path)
+        {
+            //var directoryInfo = new DirectoryInfo(path);
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+
+                Console.WriteLine($"===========================================");
+                Console.WriteLine($"Path: {path} Found and Deleted");
+                Console.WriteLine($"===========================================");
+            }
+        }
+
+        public static long GetFolderSize(string path, bool allDirectories, string extension)
+        {
+            var option = allDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            return new DirectoryInfo(path).EnumerateFiles("*" + extension, option).Sum(file => file.Length);
+        }
+
+        public long GetFolderSizeNoExtension(string path, bool allDirectories)
+        {
+            var option = allDirectories ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            return new DirectoryInfo(path).EnumerateFiles("*", option).Sum(file => file.Length);
+        }
+
+        public static string FormatFileSize(long bytes)
+        {
+            var bUnit = 1024;
+            if (bytes < bUnit)
+                return $"{bytes} B";
+
+            int exponential = (int)(Math.Log(bytes) / Math.Log(bUnit));
+            return $"{bytes / Math.Pow(bUnit, exponential):F2} {("KMGTPE")[exponential - 1]}B";
+        }
+
+        public void DeletedFolderMessage(int count, string sizeDeleted)
+        {
+            Console.WriteLine($"Deleted {count} folders");
+            Console.WriteLine($"Deleted {sizeDeleted} folders");
+            Console.WriteLine("Finished. Press any key to close this window.");
+        }
+    }
+}
---
+++ CleanObjBinFolder/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using CleanObjBinFolder;
+
+string pathToSearch = string.Empty;
+//pathToSearch = AppDomain.CurrentDomain.BaseDirectory;
+pathToSearch = Console.ReadLine();
+long sumFileDelete = 0;
+DirectoryDeleting directoryDeleting = new DirectoryDeleting();
+var listPathToDelete = directoryDeleting.PathsToDelete;
+
+Console.WriteLine($"Path to look: {pathToSearch}");
+
+if (string.IsNullOrWhiteSpace(pathToSearch))
+{
+	Console.WriteLine("Need to Add a Path");
+}
+else
+{
+	if (pathToSearch.Contains("\""))
+	{
+		pathToSearch = pathToSearch.Replace("\"", "");
+
+		if (string.IsNullOrWhiteSpace(pathToSearch))
+			Console.WriteLine("Need to Add a Path");
+	}
+	else
+	{
+		try
+		{            
+			directoryDeleting.RecursiveDirectory(pathToSearch);
+
+			foreach (var pathToDelete in listPathToDelete)
+			{
+				sumFileDelete += directoryDeleting.GetFolderSizeNoExtension(pathToDelete, true);
+                DirectoryDeleting.DeleteDirectory(pathToDelete);
+            }
+
+			directoryDeleting.DeletedFolderMessage(listPathToDelete.Count, DirectoryDeleting.FormatFileSize(sumFileDelete));
+
+        }
+		catch (Exception ex)
+		{
+			Console.WriteLine("There was an issue:");
+			Console.WriteLine(ex.Message);
+            directoryDeleting.DeletedFolderMessage(listPathToDelete.Count, DirectoryDeleting.FormatFileSize(sumFileDelete));
+        }
+	}
+}
+
+Console.ReadKey();
